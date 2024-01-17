faf8214e53fc588206421324ba26e13e90f56be6
tanya-0824 <154406184+tanya-0824@users.noreply.github.com>
2024-01-17 16:33:28 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import AuthComponent from './components/Auth';
+
+function App() {
+  return (
+    <div className="App">
+      <AuthComponent />
+    </div>
+  );
+}
+
+export default App;
